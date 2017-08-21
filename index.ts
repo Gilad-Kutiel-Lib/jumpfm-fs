98301132f30507d2a6afbed7f7f59da12de85c41
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-21 15:39:27 +0300

    view

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { JumpFm, Panel } from 'jumpfm-api'
 
-import * as fs from 'fs-extra'
+import * as fs from 'fs'
 import * as watch from 'node-watch'
 import * as path from 'path'
 
@@ -9,13 +9,11 @@ var showHiddenFiles = false
 
 class FileSystem {
     watcher = { close: () => { } }
-    readonly maxFiles: number;
     readonly panel: Panel
     readonly jumpFm: JumpFm
 
     constructor(jumpFm: JumpFm, panel: Panel) {
         this.jumpFm = jumpFm
-        this.maxFiles = jumpFm.settings.getNum('maxFiles', 1000)
         this.panel = panel
         panel.listen(this)
     }
@@ -28,28 +26,24 @@ class FileSystem {
         this.watcher = watch(url.path, { recursive: false }, this.ll)
     }
 
-    readDirAndSlice = (fullPath: string, cb: (files: string[]) => void): void => {
-        fs.readdir(fullPath, (err, items) => {
-            console.log('items')
-            if (items.length <= this.maxFiles) return cb(items)
-            this.jumpFm.statusBar.warn('fs', {
-                txt: 'fs warn',
-                dataTitle: `Showing only ${this.maxFiles} files out of ${items.length}`
-            }, 15000)
-            cb(items.slice(0, this.maxFiles))
-        })
-    }
-
     ll = () => {
         const fullPath = this.panel.getPath()
-        this.readDirAndSlice(fullPath, (files) => {
+        fs.readdir(fullPath, (err, files) => {
+            console.log('items')
             this.panel.setItems(
                 files
                     .filter(name => showHiddenFiles || name.indexOf('.') != 0)
-                    .map(name => path.join(fullPath, name))
-                    .filter(fullPath => fs.existsSync(fullPath))
-                    .map(fullPath => this.panel.itemFromPath(fullPath))
-            )
+                    .map(name => ({
+                        name: name
+                        , path: path.join(fullPath, name)
+                    }))
+                    .filter(item => fs.existsSync(item.path))
+            ).getItems().forEach(item => {
+                fs.stat(item.path, (err, stat) => {
+                    item.setTime(stat.mtime.getTime())
+                    item.setSize(stat.size)
+                })
+            })
         })
     }
 }
@@ -66,10 +60,10 @@ export const load = (jumpFm: JumpFm) => {
             })
     }
 
-    jumpFm.bindKeys('toggleHiddenFiles', ['h'], () => {
-        showHiddenFiles = !showHiddenFiles
-        fss.forEach(fs => fs.ll())
-        msg()
-    }).filterMode([])
-    msg()
+    // jumpFm.bindKeys('toggleHiddenFiles', ['h'], () => {
+    //     showHiddenFiles = !showHiddenFiles
+    //     fss.forEach(fs => fs.ll())
+    //     msg()
+    // }).filterMode([])
+    // msg()
 }
